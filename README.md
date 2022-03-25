b6ab439f7d1f2ba3dbab2976effdf8cb50714ea7
Fy_R <983242556@qq.com>
2022-03-25 11:56:23 +0800

    updata

---
+++ README.md
@@ -0,0 +1,10 @@
+# MADDPG
+
+## Requirements
+
+- python=3.6.5
+- [Multi-Agent Particle Environment(MPE)](https://github.com/openai/multiagent-particle-envs)
+- torch=1.1.0
+
+multiagent文件夹为MPE环境。无需再重复安装MPE
+
---
+++ multiagent/scenario.py
@@ -0,0 +1,11 @@
+import numpy as np
+# 包含所有场景扩展的基本场景对象
+
+# defines scenario upon which the world is built
+class BaseScenario(object):
+    # create elements of the world
+    def make_world(self):
+        raise NotImplementedError()
+    # create initial conditions of the world
+    def reset_world(self, world):
+        raise NotImplementedError()
